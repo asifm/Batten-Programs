8e5cd088099b7ea0981872b36e8701bb8b5c8064
Mehedi <MehediA@darden.virginia.edu>
2020-08-28 09:35:38 -0400

    Increase opacity of custom colors

--- colors.js
+++ colors.js
@@ -3,7 +3,7 @@ module.exports = {
   'dd-yellow': 'rgba(242, 205, 0, 1)',
   'dd-blue': 'rgba(20, 30, 60, 1)',
   'dd-lightblue': 'rgba(32, 41, 70, 1)',
-  learn: 'rgba(229, 114, 0, 0.4)',
-  experience: 'rgba(242, 205, 0, 0.4)',
-  connect: 'rgba(157, 214, 25, 0.4)',
+  learn: 'rgba(229, 114, 0, 0.8)',
+  experience: 'rgba(242, 205, 0, 0.8)',
+  connect: 'rgba(157, 214, 25, 0.8)',
 };
