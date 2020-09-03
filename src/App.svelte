84b7e5f4084297571562a2709ee53d2db6fec986
Mehedi <MehediA@darden.virginia.edu>
2020-09-03 07:33:42 -0400

    Fix toggle and clean up

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
   import { fade, fly } from 'svelte/transition';
 
   import Tailwindcss from './Tailwindcss.svelte';
-  import Roadmap from './Components/Roadmap/Roadmap.svelte';
+  // import Roadmap from './Components/Roadmap/Roadmap.svelte';
   import ProgramTile from './Components/ProgramTile/ProgramTile.svelte';
   import ProgramMonthTile from './Components/ProgramMonthTile/ProgramMonthTile.svelte';
   import { dataPromise } from './data/parseAirtableData';
+
   let programs = [];
   $: selectedPrograms = programs;
+
   let programTypes = ['All Types'];
   let programMonths = [];
   let dataready = false;
@@ -21,7 +23,6 @@
 
   function handleDropdownOptionClick(e) {
     let optSelected = e.target.innerText;
-    console.log(optSelected);
     hiddenDropdown = true;
     if (optSelected === 'All Types') {
       selectedPrograms = programs;
@@ -29,15 +30,19 @@
       selectedPrograms = programs.filter(el => el.programType == optSelected);
     }
   }
-  function handleToggleClick() {
+  function handleAlumniToggle() {
     alumniToggle = !alumniToggle;
-    console.log(alumniToggle);
     if (alumniToggle) {
       selectedPrograms = programs.filter(el => el.alumni);
     } else {
       selectedPrograms = programs;
     }
   }
+
+  function handleTileClick(e, programData) {
+    console.log(e, programData);
+  }
+
   dataPromise.then(resolvedData => {
     // Using the technique found here https://stackoverflow.com/a/39333479/1927583
     programs = resolvedData[0].map(el =>
@@ -79,30 +84,26 @@
 <Tailwindcss />
 <main>
   <!-- header -->
-  <div class="container mx-auto">
-    <h2 class="title">
-      <span class="px-3 rounded shadow-sm bg-learn">Learn</span>
-      <span class="px-3 rounded shadow-sm bg-experience">Experience</span>
-      <span class="px-3 rounded shadow-sm bg-connect">Connect</span>
-    </h2>
+  <div class="mb-3 leading-tight text-center shadow bg-tangerine text-dd-blue">
+    <div class="container px-4 py-1 mx-auto">
+      <h2>Batten Institute Programs</h2>
+      <h3 class="">Fall 2020–21</h3>
+    </div>
   </div>
 
   <!-- New section: ProgramMonthTiles -->
-  <!-- todo: add dropdown for program types -->
-  <!-- todo: add toggle for "open to alumni" -->
-  <div class="container px-3 mx-auto">
-    <p class="my-10 text-gray-700 border-t">[View: Small Cards by Month]</p>
 
+  <div class="container px-3 mx-auto">
     {#if dataready}
-      <div class="grid grid-cols-2 mb-10 lg:grid-cols-6">
+      <div class="grid grid-cols-2 gap-3 mb-10">
         <!-- Dropdown button -->
-        <div class="relative col-span-1 col-start-1 text-left" id="selecttype">
+        <div class="relative col-span-1 text-left">
           <div>
             <span class="rounded-md shadow-sm">
               <button
                 type="button"
                 on:click={handleDropdownButtonClick}
-                class="inline-flex justify-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                class="inline-flex px-4 py-2 font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
                 id="options-menu"
                 aria-haspopup="true"
                 aria-expanded="true">
@@ -123,7 +124,7 @@
             class="absolute right-0 w-56 mt-2 origin-top-right rounded-md shadow-lg">
             <div class="bg-white rounded-md">
               <div
-                class="py-1"
+                class="py-1 cursor-pointer"
                 role="menu"
                 hidden={hiddenDropdown}
                 on:click={handleDropdownOptionClick}
@@ -140,29 +141,23 @@
         </div>
 
         <!-- Toggle -->
-        <!-- todo: make toggle work -->
-        <div class="col-span-1 col-start-6">
-          <span class="space-x-2">Programs open to alumni</span>
+
+        <div class="col-span-1 text-right">
+          <span class="p-1 text-sm text-gray-700 rounded">Open to alumni</span>
           <span
             role="checkbox"
             tabindex="0"
-            aria-checked={alumniToggle}
-            on:click={handleToggleClick}
-            class="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 space-x-2 cursor-pointer group focus:outline-none">
-            <!-- On: "bg-indigo-600", Off: "bg-gray-200" -->
-            <!-- bg not working -->
+            aria-checked="false"
+            on:click={handleAlumniToggle}
+            class="align-middle relative inline-flex flex-shrink-0 h-6
+              transition-colors duration-200 ease-in-out {alumniToggle ? 'bg-dd-blue-600' : 'bg-gray-200'}
+              border-2 border-transparent rounded-full cursor-pointer w-16 focus:outline-none
+              focus:shadow-outline">
             <span
               aria-hidden="true"
-              class="{alumniToggle ? 'bg-dd-blue' : ' bg-gray-300'} absolute h-4
-                mx-auto transition-colors duration-200 ease-in-out rounded-full inline-block
-                w-9" />
-            <!-- On: "translate-x-5", Off: "translate-x-0" -->
-            <span
-              aria-hidden="true"
-              class="{alumniToggle ? 'translate-x-5 ' : 'translate-x-0 '} absolute
-                left-0 inline-block w-5 h-5 transition-transform duration-200 ease-in-out
-                transform translate-x-0 bg-white border border-gray-200 rounded-full
-                shadow group-focus:shadow-outline group-focus:border-blue-300" />
+              class="inline-block w-6 h-6 transition duration-200 ease-in-out
+                transform {alumniToggle ? 'translate-x-10' : 'translate-x-0'} bg-white
+                rounded-full shadow" />
           </span>
         </div>
       </div>
@@ -170,7 +165,8 @@
       <div class="grid grid-cols-1 sm:gap-5 sm:grid-cols-2 lg:grid-cols-5">
         {#each programMonths as month}
           <div class="col-span-1">
-            <div class="p-3 font-bold tracking-widest rounded-md text-dd-blue">
+            <div
+              class="p-3 font-bold tracking-widest uppercase bg-gray-100 rounded-md text-dd-blue">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -188,6 +184,7 @@
             {#each selectedPrograms as programData}
               <li
                 class="flex flex-col rounded-md"
+                on:click={event => handleTileClick(event, programData)}
                 in:fly|local={{ y: 100, duration: 750 }}
                 out:fade|local>
                 {#if programData.months.includes(month)}
@@ -200,23 +197,9 @@
       </div>
     {/if}
   </div>
-  <!-- New section: Roadmap -->
-  <div class="container mx-auto">
-    <p class="my-10 text-gray-700 border-t">[View: Gantt-Like]</p>
-    <Roadmap />
-  </div>
 
   <!-- New section: ProgramTiles -->
   <div class="container mx-auto">
-    <p class="my-10 text-gray-700 border-t">[View: Cards]</p>
-    <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {#if dataready}
-        {#each programs as program}
-          <li class="col-span-1 bg-white rounded-lg shadow">
-            <ProgramTile {...program} />
-          </li>
-        {/each}
-      {/if}
-    </ul>
+    <!-- <ProgramTile {...programData} /> -->
   </div>
 </main>
