3421eea144bc8ee9e0f68833825077290a2376b8
Asif Mehedi <asif.mehedi@live.com>
2020-09-06 13:52:10 -0400

    Re-add sort to program types

--- src/data/parseAirtableData.js
+++ src/data/parseAirtableData.js
@@ -81,7 +81,9 @@ function parseData(data) {
       }
     }
   });
-  programTypesArr = [...new Set(outputArr.map(el => el.programType).flat())];
+  programTypesArr = [
+    ...new Set(outputArr.map(el => el.programType).flat()),
+  ].sort();
   // todo: make it more robust to account for correct sorting, year etc.
   programMonthsArr = [...new Set(outputArr.map(el => el.months).flat())];
 
