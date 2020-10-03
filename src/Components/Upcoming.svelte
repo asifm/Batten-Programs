038c36792250323614d6f600e86d24cd3b4a0418
Asif Mehedi <asif.mehedi@live.com>
2020-10-03 14:23:18 -0400

    Change bg in "Upcoming"

--- src/Components/Upcoming.svelte
+++ src/Components/Upcoming.svelte
@@ -15,30 +15,18 @@
   });
 </script>
 
-<div class="p-5">
-  <h3 class="text-2xl text-dd-orange">Upcoming Programs and Events</h3>
+<div class="p-4">
+  <h2 class="text-3xl text-dd-orange">Upcoming Programs and Events</h2>
   {#each selectedPrograms as program, i}
     <a href={program.link} target="_blank">
       <div
-        class="pb-2 mb-2 duration-300 transform border-b rounded shadow-sm cursor-pointer group hover:shadow-lg hover:bg-dd-orange">
-        <div
-          class="p-2 font-bold tracking-widest uppercase rounded"
-          style="background-color:{program.programTypeColor}">
-          {program.name}
-        </div>
-
-        <div class="px-2 mt-1">
-          <span
-            class="border border-dd-blue-200 bg-yellow-100 px-1.5 py-0.5
-              tracking-widest text-xs text-black uppercase rounded-bl-md">{getFormattedDateFromEpoch(program.start)}
-            –
-            {getFormattedDateFromEpoch(program.end)}</span>
-
-          <span
-            class="border border-dd-orange px-1.5 py-0.5 tracking-widest text-xs
-              text-dd-blue uppercase bg-dd-orange-100 rounded-br-md">{program.programType}</span>
-          <svg
-            class="inline-block w-5 h-5 ml-3 text-dd-blue-300"
+        class="mb-2 duration-200 border rounded shadow-sm cursor-pointer group hover:shadow-lg hover:border-dd-blue-300"
+        style="background-color:{program.programTypeColor}">
+        <div class="px-3 pt-2">
+          <span class="font-bold tracking-widest uppercase rounded">
+            {program.name}
+          </span><svg
+            class="inline-block w-5 h-5 text-dd-blue-400"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"><path
@@ -46,8 +34,22 @@
             <path
               d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" /></svg>
         </div>
-        <div class="px-2 duration-500 group-hover:text-white">
-          <p>{program.quickDescription}</p>
+
+        <div class="p-3">
+          <div class="flex">
+            <span
+              class="border border-dd-blue-300 bg-yellow-100 px-1.5 py-0.5
+                tracking-widest text-xs text-black uppercase rounded-bl-md">{getFormattedDateFromEpoch(program.start)}
+              –
+              {getFormattedDateFromEpoch(program.end)}</span>
+
+            <span
+              class="border border-dd-orange px-1.5 py-0.5 tracking-widest text-xs
+                text-dd-blue uppercase bg-dd-orange-100 rounded-br-md">{program.programType}</span>
+          </div>
+          <div>
+            <p>{program.quickDescription}</p>
+          </div>
         </div>
       </div>
     </a>
