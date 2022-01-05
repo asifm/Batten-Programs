372e57cec5eacc6861c606ccb1078299dd0a3b7c
Asif Mehedi <asif.mehedi@live.com>
2022-01-05 17:37:14 -0500

    Clean up unused colors

--- src/data/colors.js
+++ src/data/colors.js
@@ -13,40 +13,8 @@ exports.customColors = {
 	// https://www.htmlcsscolor.com/hex/e57200
 	'dd-orange': {
 		100: '#FCF1E6',
-		200: '#F9DCBF',
-		300: '#F5C799',
-		400: '#ED9C4D',
 		DEFAULT: '#E57200',
 		alt: '#eb5f0c',
 	},
-	// https://www.htmlcsscolor.com/hex/84bd00
-	citrus: '#9dd619',
-	// https://www.htmlcsscolor.com/hex/642f6c
-	seance: '#642f6c',
-	// https://www.htmlcsscolor.com/hex/f2cd00
 	tangerine: '#f2cd00',
-	// https://www.htmlcsscolor.com/hex/141e3c
-	midnight: '#141e3c',
-	lightmidnight: '#cdd0d6',
-	// https://www.htmlcsscolor.com/hex/cac0b6
-	stark: '#cac0b6',
-	// https://www.htmlcsscolor.com/hex/007681
-	tealy: '#007681',
-	// Theme colors
-	learn: '#F5C799', // dd-orange-300
-	experience: '#f9e57d', // light tangerine
-	connect: '#cdea89', // light citrus
 };
-
-// Probably not used anywhere. Todo: delete after confirming it's not needed.
-exports.palette = [
-	'#80deea',
-	'#ffab91',
-	'#ce93d8',
-	'#90a4ae',
-	'#f2cd00',
-	'#ed9c4d',
-	'#9dd619',
-	'#a5d6a7',
-	'#afb42b',
-];
