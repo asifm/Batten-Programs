48ba15f38a2059fdb6b13feb3b0c7fe2f1213fd9
Asif Mehedi <asif.mehedi@live.com>
2023-08-03 00:09:04 -0400

    Remove all and upcoming components and routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,5 @@
 <script>
     import router from "page";
-    import Upcoming from "./Components/Upcoming.svelte";
-    import All from "./Components/All.svelte";
-    // New imports
     import BattenFull from "./Components/BattenFull.svelte";
     import BattenFeatured from "./Components/BattenFeatured.svelte";
     import MayoFull from "./Components/MayoFull.svelte";
@@ -10,9 +7,6 @@
     import VCFull from "./Components/VCFull.svelte";
 
     let page;
-    router("/upcoming", () => (page = Upcoming));
-    router("/all", () => (page = All));
-    // New endpoints
     router("/batten-full", () => (page = BattenFull));
     router("/batten-featured", () => (page = BattenFeatured));
     router("/mayo-full", () => (page = MayoFull));
